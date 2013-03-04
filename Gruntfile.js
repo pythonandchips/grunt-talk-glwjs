961a7c312b4f633b7a4b6898ed77af4ea04343ab
Colin Gemmell <pythonandchips@gmail.com>
2013-03-04 21:24:00 +0000

    added release target

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,30 @@ module.exports = function(grunt) {
         specs : 'spec/**/*Spec.js',
         helpers : 'spec/helpers/*.js'
       }
+    },
+    concat: {
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/all.min.js'
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/all.min.js': ['dist/all.min.js']
+        }
+      }
+    },
+    clean: {
+      release: ['dist/**/*.js']
     }
   });
 
   grunt.registerTask('default', ['jshint', 'jasmine']);
+  grunt.registerTask('release', ['jshint', 'clean', 'concat', 'uglify']);
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 };
